aef5ed342d7397577b110b47ce004e52f7f97d4c
Gabriel Ong <117062305+gongahkia@users.noreply.github.com>
2022-12-24 22:35:46 +0800

    Add files via upload
    (cherry picked from commit 51e29cb7917141af1da40f08e845068908565be6)

---
+++ 2019/day1/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+#include <fstream>
+ 
+using namespace std;
+
+
+// to parse each individual line of the string
+int main () {
+    string line;
+    int part1totalsum = 0;
+    int part2totalsum = 0;
+    int part2tempsum = 0;
+    int tempsum;
+    int tempsum2;
+    ifstream fhand ("input.txt");
+  
+    while (getline (fhand, line)) {
+        // PART 1
+        tempsum = 0;
+        tempsum = floor(stoi(line)/3)-2;
+        part1totalsum += tempsum;
+    };
+    cout << "Part 1: " << part1totalsum << endl;
+}
+
+   
