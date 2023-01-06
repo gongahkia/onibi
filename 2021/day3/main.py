9281f328b171134884e7f526aac5c445a5bb71a0
Gabriel Ong <117062305+gongahkia@users.noreply.github.com>
2023-01-06 22:40:10 +0800

    solved part 2 bless
    (cherry picked from commit 81824393396f4f34bf013deae7bf9409c326789b)

--- 2021/day3/main.py
+++ 2021/day3/main.py
@@ -19,5 +19,124 @@ def part1():
         else:
             gamma_rate += '1'
             epsilon_rate += '0'
-    return (int(gamma_rate,2) * int(epsilon_rate,2))          
-print(part1())
+    return (int(gamma_rate,2) * int(epsilon_rate,2))
+
+def Oxygen():
+    i = 0
+    Oxygendecimal = 0
+    data_storer = []
+    fhand = open("input.txt","r")
+    for line in fhand:
+        line = line.rstrip("\n")
+        data_storer.append(line)
+
+    Oxygen_data_storer = data_storer
+
+    #recurse until the number of items in data_storer == 1, and edit the list directly to save changes across iterations
+    for char in Oxygen_data_storer[0]:
+
+        if len(Oxygen_data_storer) == 1:
+            break
+
+        Oxygen_toremove: string
+        column_storer = {}
+
+        #creation of column per iteration to track more common/less common bits
+        for row in Oxygen_data_storer:
+            count = 0
+            for char in row:
+                column_storer[count] = column_storer.get(count,"") + char
+                count += 1
+
+        indent = 0
+
+        if column_storer[i].count("0") > column_storer[i].count("1"):
+            Oxygen_toremove = "1"
+        elif column_storer[i].count("1") > column_storer[i].count("0"):
+            Oxygen_toremove = "0"
+        else:
+            Oxygen_toremove = "0"
+        
+        if Oxygen_toremove == "1":
+            for itervar in range(len(column_storer[i])):
+                if (column_storer[i])[itervar] == "1":
+                    del Oxygen_data_storer[itervar-indent]
+                    indent += 1
+
+        elif Oxygen_toremove == "0":
+            for itervar in range(len(column_storer[i])):
+                if (column_storer[i])[itervar] == "0":
+                    del Oxygen_data_storer[itervar-indent]
+                    indent += 1
+
+        else:
+            print("Edge case")
+
+        i += 1
+
+    Oxygendecimal = int(Oxygen_data_storer[0],2)
+    return Oxygendecimal
+
+
+
+def CO2():
+    q = 0
+    Carbondecimal = 0
+    data_storer = []
+    fhand = open("input.txt","r")
+    for line in fhand:
+        line = line.rstrip("\n")
+        data_storer.append(line)
+
+    Carbon_data_storer = data_storer
+
+    for char in Carbon_data_storer[0]:
+        
+        if len(Carbon_data_storer) == 1:
+            break
+
+        Carbon_toremove: string
+        column_storer = {}
+
+        for row in Carbon_data_storer:
+            count = 0
+            for char in row:
+                column_storer[count] = column_storer.get(count,"") + char
+                count += 1
+
+        carbonindent = 0 
+        
+        if column_storer[q].count("0") > column_storer[q].count("1"):
+            Carbon_toremove = "0"
+        elif column_storer[q].count("1") > column_storer[q].count("0"):
+            Carbon_toremove = "1"
+        else:
+            Carbon_toremove = "1"
+ 
+        if Carbon_toremove == "1":
+            for itervar in range(len(column_storer[q])):
+                if (column_storer[q])[itervar] == "1":
+                    del Carbon_data_storer[itervar-carbonindent]
+                    carbonindent += 1
+
+        elif Carbon_toremove == "0":
+            for itervar in range(len(column_storer[q])):
+                if (column_storer[q])[itervar] == "0":
+                    del Carbon_data_storer[itervar-carbonindent]
+                    carbonindent += 1
+
+        else:
+            print("Edge case")
+        
+        q += 1
+
+
+    Carbondecimal = int(Carbon_data_storer[0],2)
+    
+    return (Carbondecimal)
+
+def part2():
+    return (Oxygen() * CO2())
+
+print(f"part 1: {part1()}")
+print(f"part 2: {part2()}")
