8d46119cdfa4c82775bffbaa775e46b39fce3fbb
Gabriel Ong <117062305+gongahkia@users.noreply.github.com>
2023-01-12 21:56:02 +0800

    find it hilarious that I couldn't even finish part 1 in CPP or JULIA
    (cherry picked from commit e08aa839b4ff2ef378b6f3c5dcee6ae86c1f3bd3)

---
+++ 2016/day2/day2.py
@@ -0,0 +1,59 @@
+class movement:
+
+    def __init__(self, coord = [0,0]):
+        print("Movement body is instantiated!")
+        self.coordinates = coord
+
+    def up(self):
+        self.coordinates[1] += 1
+    
+    def down(self):
+        self.coordinates[1] -= 1
+
+    def left(self):
+        self.coordinates[0] -= 1
+
+    def right(self):
+        self.coordinates[0] += 1
+
+    def checkbounds(self):
+        if self.coordinates[0] < -1 or self.coordinates[0] > 1:
+            return True
+        if self.coordinates[1] < -1 or self.coordinates[1] > 1:
+            return True
+        else:
+            return False
+
+def part1():
+    templist = []
+    fhand = open("input.txt","r")
+    for line in fhand:
+        line = line.rstrip("\n")
+        templist.append(line)
+    
+    a = movement()
+
+    for item in templist:
+        for char in item:
+            if char == "D":
+                a.down()
+                if a.checkbounds() == True:
+                    a.up()
+            elif char == "U":
+                a.up()
+                if a.checkbounds() == True:
+                    a.down()
+            elif char == "L":
+                a.left()
+                if a.checkbounds() == True:
+                    a.right()
+            elif char == "R":
+                a.right()
+                if a.checkbounds() == True:
+                    a.left()
+        print(a.coordinates)
+        #dictionaries weren't working for me today, no idea why tbh
+
+def part2():
+
+part1()
