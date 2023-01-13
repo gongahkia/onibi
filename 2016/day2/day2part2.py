6a496457d74cb2540ef383cf7ea9ca63c6eaed1c
Gabriel Ong <117062305+gongahkia@users.noreply.github.com>
2023-01-13 16:10:24 +0800

    def could have just used a single if else statement for part 1
    (cherry picked from commit 86a0dc3171c24363f6f32568954d50b72f96ffa5)

---
+++ 2016/day2/day2part2.py
@@ -0,0 +1,58 @@
+class movement:
+
+    def __init__ (self, location = [0,0]):
+        print("Movement object is instantiated")
+        self.location = location
+        #hashable object, list
+
+    def up(self):
+        self.location[1] += 1
+
+    def down(self):
+        self.location[1] -= 1
+
+    def right(self):
+        self.location[0] += 1
+
+    def left(self):
+        self.location[0] -= 1
+
+    def checksum(self):
+        if self.location == [0,3] or self.location == [-1,2] or self.location == [-2,1] or self.location == [-3,0] or self.location == [-2,-1] or self.location == [-1,-2] or self.location == [0,-3] or self.location == [1,-2] or self.location == [2,-1] or self.location == [3,0] or self.location == [2,1] or self.location == [1,2]:
+            return True
+        else:
+            return False
+
+
+
+
+
+
+def part2():
+    templist = []
+    fhand = open("input.txt","r")
+    for line in fhand:
+        line = line.rstrip("\n")
+        templist.append(line)
+    b = movement()
+    for instruction in templist:
+        for char in instruction:
+            if char == 'U':
+                b.up()
+                if b.checksum() == True:
+                    b.down()
+            if char == 'D':
+                b.down()
+                if b.checksum() == True:
+                    b.up()
+            if char == 'L':
+                b.left()
+                if b.checksum() == True:
+                    b.right()
+            if char == 'R':
+                b.right()
+                if b.checksum() == True:
+                    b.left()
+        print(b.location)
+
+part2()
