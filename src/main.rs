7aa7cd3ccb752b81df9622ddf568cb0b4a0f006c
gongahkia <angryapplegravy@gmail.com>
2023-05-01 16:57:48 +0800

    new changes
    (cherry picked from commit ada5378ddc04188850a36390035e1f77f02788f9)

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 // to resolve:
 // - if needed, do the following in a separate isolated rust file
     // - destructure the data presented into the Task struct if possible, otherwise find some way to utilise the struct which is currently quite useless
-    // - add error checking for invalid input for:
-        // the date 
-        // the urgency level -- done 
+
+// to add
+// - if needed, do the following in a separate isolated rust file
+    // - saving local file data to another file, and loading a previous save (reading and writing to files)
+    // - modularize the functions used in this program to prevent it from becoming one massive file
+// - ability to edit items
+
+// ----------
 
 use std::io;
 
@@ -24,12 +29,12 @@ enum UrgencyLevel {
 
 fn main() {
 
-    let mut storage_array:Vec<(String, String, [u32;3], UrgencyLevel)> = vec![];
+    let mut storage_array:Vec<(String, String, [i32;3], UrgencyLevel)> = vec![];
 
     loop {
         
         // break condition
-        println!("[E]xit: ");
+        println!("[E]xit / [Enter] to add task: ");
         let mut exit_condition:String = String::new();
         io::stdin().read_line(&mut exit_condition).expect("Failed to read line");
         let exit_condition_str:&str = exit_condition.as_str().trim_end();
@@ -50,20 +55,58 @@ fn main() {
         let userinput_task_description = String::from(userinput_task_description.trim_end());
 
         // task deadline -> parsed using destructuring
+            // for future reference:
+            // error was initially occuring due to newline character of last element in vector, need
+            // to remember to use .trim_end() method to remove said newline character
         println!("Enter task deadline in the following format [DD/MM/YY]: ");
-        let mut userinput_task_deadline_raw:String = String::new();
-        io::stdin().read_line(&mut userinput_task_deadline_raw).expect("Failed to read line");
-        let userinput_task_deadline_raw_array = userinput_task_deadline_raw.split("/");
-        let userinput_task_deadline_array: Vec<&str> = userinput_task_deadline_raw_array.collect();
-        
-        // for future reference:
-        // error was initially occuring due to newline character of last element in vector, need
-        // to remember to use .trim_end() method to remove said newline character
-        let userinput_task_deadline_day_int:u32 = userinput_task_deadline_array[0].trim_end().parse().unwrap();
-        let userinput_task_deadline_month_int:u32 = userinput_task_deadline_array[1].trim_end().parse().unwrap();
-        let userinput_task_deadline_year_int:u32 = userinput_task_deadline_array[2].trim_end().parse().unwrap();
+        let userinput_task_deadline_formatted:[i32; 3];
 
-        let userinput_task_deadline_formatted:[u32; 3] = [userinput_task_deadline_day_int, userinput_task_deadline_month_int, userinput_task_deadline_year_int];
+        loop {
+            let mut userinput_task_deadline_raw:String = String::new();
+            io::stdin().read_line(&mut userinput_task_deadline_raw).expect("Failed to read line");
+            let userinput_task_deadline_raw_array = userinput_task_deadline_raw.split("/");
+            let userinput_task_deadline_array: Vec<&str> = userinput_task_deadline_raw_array.collect();
+            
+            // checking for valid number of fields input (characters, str literals and numbers covered)
+            if userinput_task_deadline_array.len() != 3 {
+                println!("Invalid input detected.\nEnter task deadline in the following format [DD/MM/YY]: ");
+                continue;
+            }
+
+            // checking for characters instead of date input if there are 3 fields
+                // for future reference:
+                // here, we made use of the .chars(), .all() operator, which is extremely powerful
+                // for checking whether multiple items satisfy a predicate
+            if userinput_task_deadline_array[0].chars().all(char::is_numeric) && userinput_task_deadline_array[1].chars().all(char::is_numeric) && userinput_task_deadline_array[2].trim_end().chars().all(char::is_numeric) {
+                // println!("awesome");
+            } else {
+                // println!("fk off");
+                println!("Enter a valid integer input.\nEnter task deadline in the following format [DD/MM/YY]: ");
+                continue;
+            }
+
+            // these have to be signed integers first, to allow for subsequent error checking
+            let userinput_task_deadline_day_int:i32 = userinput_task_deadline_array[0].trim_end().parse().unwrap();
+            let userinput_task_deadline_month_int:i32 = userinput_task_deadline_array[1].trim_end().parse().unwrap();
+            let userinput_task_deadline_year_int:i32 = userinput_task_deadline_array[2].trim_end().parse().unwrap();
+            
+            // checking for valid date inputs
+            if userinput_task_deadline_day_int > 31 || userinput_task_deadline_day_int < 1 {
+                println!("Enter a valid day input.\nEnter task deadline in the following format [DD/MM/YY]: ");
+                continue;
+            }
+            if userinput_task_deadline_month_int > 12 || userinput_task_deadline_month_int < 1 {
+                println!("Enter a valid month input.\nEnter task deadline in the following format [DD/MM/YY]: ");
+                continue;
+            } 
+            if userinput_task_deadline_year_int < 23 || userinput_task_deadline_year_int > 99 {
+                println!("Enter a valid year input.\nEnter task deadline in the following format [DD/MM/YY]: ");
+                continue; 
+            }
+            userinput_task_deadline_formatted = [userinput_task_deadline_day_int, userinput_task_deadline_month_int, userinput_task_deadline_year_int];
+            break;
+            // let [user_day, user_month, user_year] = userinput_task_deadline_formatted;
+        }
 
         // task urgency handled by an enum
         println!("Enter task urgency (L/M/H): ");
