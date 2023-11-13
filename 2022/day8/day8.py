e21447ad192647262225eadb0d27e74cc8a00b6c
gongahkia <angryapplegravy@gmail.com>
2023-11-13 16:11:51 +0800

    shit
    (cherry picked from commit 691ee1e120ed3c4407e4c96478b48e5bdabaa812)

--- 2022/day8/day8.py
+++ 2022/day8/day8.py
@@ -1,44 +1,27 @@
-fhand = open("input2.txt", "r")
+# True means visible 
+# False means not visible
+# to be updated later when looking via columns
+coord:{(int):bool} = {}
 
-print("\n")
-
-row_collector = {}
-row_num = 1
-
-column_collector = {}
-column_num = 1
-
-for line in fhand:
-    print(line, end="")
-
-# to create the row
-    row_collector[row_num] = line.rstrip("\n")
-    row_num += 1
-
-# to create the column
-for row in row_collector:
-    column_num = 1
-    for tree in row_collector[row]:
-        column_collector[column_num] = column_collector.get(column_num, "") + tree
-        column_num += 1
-
-print("\n\n--------------------\n")
-
-print(f"columns: {column_collector}")
-print(f"rows: {row_collector}\n")
-
-total_left_count = 0
-total_right_count = 0
-top_count = 0
-bottom_count = 0
-
-for row in row_collector:
-    left_count = 1
-    for itervar in range(len(row_collector[row])):
-        if row_collector[row][itervar+1] <= row_collector[row][itervar]:
-            break
-        else:
-            left_count += 1
-    total_left_count += left_count
-
-print(f"left count: {total_left_count}")
+def parta():
+    fhand = open("input.txt","r")
+    data_array:[int] = [line.rstrip() for line in fhand]
+    fhand.close()
+    
+    for y in range(len(data_array)):
+        for x in range(len(data_array[y])):
+            if x > len(data_array[y]):
+                break
+            else:
+                if data_array[y][x] >= data_array[y][x+1]:
+                    coord[(x,y)] = True
+                    coord[(x+1,y)] = False
+                    break
+                elif data_array[y][x] < data_array[y][x+1]:
+                    coord[(x,y)] = True
+                    continue
+                else:
+                    return "edge case 001"
+    return coord
+           
+print(parta())
