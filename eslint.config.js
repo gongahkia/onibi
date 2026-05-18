82a5e87f77ff0b92f0b6bc56814450993c491eeb
Gabriel Ong Zhe Mian <angryapplegravy@gmail.com>
2026-05-18 17:33:28 +0800

    chore(monorepo): add typescript workspace tooling
    (cherry picked from commit 6b448b7b37d018b12c1bbc98a28f5dc552d51516)

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import tseslint from "typescript-eslint";
+
+export default [
+  {
+    ignores: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/coverage/**",
+      ".zig-cache/**",
+      "target/**",
+      "zig-out/**"
+    ]
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    },
+    rules: {
+      "@typescript-eslint/consistent-type-imports": ["error", { fixStyle: "inline-type-imports" }]
+    }
+  },
+  {
+    files: ["**/*.tsx"],
+    plugins: {
+      "react-hooks": reactHooks
+    },
+    rules: reactHooks.configs.recommended.rules
+  }
+];
