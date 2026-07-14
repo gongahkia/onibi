4418947496a3fb290cd13e300e2d7505d68037a4
Gabriel Ong Zhe Mian <angryapplegravy@gmail.com>
2026-07-14 11:31:54 +0800

    Define encrypted message envelope schema
    (cherry picked from commit 87e3849a8a24099feaf85277987f3a9860e9a0c3)

--- crates/yeokcham-protocol/src/lib.rs
+++ crates/yeokcham-protocol/src/lib.rs
@@ -1,9 +1,13 @@
 #![forbid(unsafe_code)]
 
 mod domain;
+mod encrypted_message;
 mod version;
 mod wire;
 
 pub use domain::CryptoDomain;
+pub use encrypted_message::{
+    EncryptedMessageEnvelope, EncryptedMessageError, MAX_ENCRYPTED_HEADER_BYTES,
+};
 pub use version::{ProtocolVersion, VersionRange};
 pub use wire::{EnvelopeKind, WireEnvelope, WireError, WireLimits};
