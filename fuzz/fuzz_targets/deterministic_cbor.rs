d88289b0c55e815b918dfb8be0ad0ecfe8cb9590
Gabriel Ong Zhe Mian <angryapplegravy@gmail.com>
2026-07-15 14:20:22 +0800

    Fuzz deterministic CBOR decoders
    (cherry picked from commit 57a442ebfbd6fac6f60a42e06d0e6f4d6930723c)

---
+++ fuzz/fuzz_targets/deterministic_cbor.rs
@@ -0,0 +1,19 @@
+#![no_main]
+
+use libfuzzer_sys::fuzz_target;
+use yeokcham_protocol::{
+    DeliveryProfile, DirectProfileConfig, ExtensionFrame, LocalMeshProfileConfig, MessagePayload,
+    RecipientCapability, TorMaildropProfileConfig, VersionNegotiation, WireEnvelope, WireLimits,
+};
+
+fuzz_target!(|data: &[u8]| {
+    let _ = DeliveryProfile::decode(data);
+    let _ = DirectProfileConfig::decode(data);
+    let _ = ExtensionFrame::decode(data);
+    let _ = LocalMeshProfileConfig::decode(data);
+    let _ = MessagePayload::decode(data);
+    let _ = RecipientCapability::decode(data);
+    let _ = TorMaildropProfileConfig::decode(data);
+    let _ = VersionNegotiation::decode(data);
+    let _ = WireEnvelope::decode(data, WireLimits::REFERENCE);
+});
