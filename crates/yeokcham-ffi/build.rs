c1979776a0252cc029a0e271907c379c43562089
Gabriel Ong Zhe Mian <angryapplegravy@gmail.com>
2026-07-20 13:53:19 +0800

    editedversioning
    (cherry picked from commit 5d2165bdf1f80122a7c5ab124ffb9552c228103f)

--- crates/yeokcham-ffi/build.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    println!("cargo:rerun-if-changed=include/yeokcham.h");
-    println!("cargo:rerun-if-changed=tests/c_consumer.c");
-    cc::Build::new()
-        .file("tests/c_consumer.c")
-        .include("include")
-        .flag_if_supported("-std=c11")
-        .compile("yeokcham_c_consumer");
-}
